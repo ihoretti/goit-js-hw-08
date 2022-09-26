881a2e9b30716165ee235cc0211dda93425cdfa2
IHOR <97735057+ihoretti@users.noreply.github.com>
2022-09-26 15:54:27 +0300

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,26 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+console.log(galleryItems);
+
+const getGallery = document.querySelector('.gallery');
+
+const getGalleryItems = galleryItems
+  .map(({ preview, original, description }) => {
+    console.log(preview);
+    return `<a class="gallery__item"  href="${original}">
+                <img
+                  class="gallery__image"
+                  src="${preview}"
+                  alt="${description}"
+                />
+          </a>`;
+  })
+  .join('');
+
+getGallery.insertAdjacentHTML('afterbegin', getGalleryItems);
+
+let lightbox = new SimpleLightbox('.gallery .gallery__item', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
