3aebe6d8acd7cbdccf573c378b3ce4d47e469a7c
IHOR <97735057+ihoretti@users.noreply.github.com>
2022-09-26 16:11:56 +0300

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -8,7 +8,6 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
-console.log(galleryItems);
 
 const getGallery = document.querySelector('.gallery');
 
