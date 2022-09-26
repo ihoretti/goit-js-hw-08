edac91c816259389a4e59d801abd8c2d7e576a47
IHOR <97735057+ihoretti@users.noreply.github.com>
2022-09-26 16:04:18 +0300

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,3 +1,8 @@
+// Описан в документации
+import SimpleLightbox from 'simplelightbox';
+// Дополнительный импорт стилей
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
